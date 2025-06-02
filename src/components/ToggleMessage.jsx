ff6e69ff6105b7a0768652bd6955ae525e3924e1
Pranusha <narsicloud@gmail.com>
2025-06-02 14:14:27 -0500

    message changed

--- src/components/ToggleMessage.jsx
+++ src/components/ToggleMessage.jsx
@@ -5,13 +5,13 @@ const ToggleMessage = () => {
 
     const [isVisible, setIsVisible] = useState(false)
 
-   const toggleMessage = () =>{
+   const message = () =>{
         setIsVisible(!isVisible);
     }
 
     return (
         <div>
-            <button onClick={toggleMessage}>{isVisible ? 'Hide' : 'Show'}</button>
+            <button onClick={message}>{isVisible ? 'Hide' : 'Show'}</button>
 
             {isVisible && <p>Hello, This is a toggle message</p>}
         </div>
